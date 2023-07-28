90fc5ba7811632cedebfda32a70916a7b1fea7ac
test <test>
2023-07-28 03:04:18 +0000

    update collator

--- spelling_correction_v2/collator.py
+++ spelling_correction_v2/collator.py
@@ -47,16 +47,19 @@ class DataCollatorCustom:
                 sentence = re.sub(r"[^0-9a-zA-Z' -]+","", sentence)
                 self.sentence_list.append(sentence)
                 
-        for sentence in self.sentence_list:
+        for i, sentence in enumerate(self.sentence_list):
             # Make misspelling sentence
             corrupted = make_misspelling(sentence, self.threshold)
 
             # Format the inputs
-#             tokens, masks, labels = self.formatting(corrupted, sentence)
-            labels = self.formatting(sentence)
-            tokens_list.append(corrupted + "[SEP]")
-#             attention_masks_list.append(masks)
+            tokens, masks, labels = self.formatting(corrupted, sentence)
+#             labels = self.formatting(sentence)
+            tokens_list.append(tokens)
+            attention_masks_list.append(masks)
             labels_list.append(labels)
+            
+#             if i == 1:
+#                 break
 
 #         sentences = [self.tokenizer.decode(tokens) for tokens in tokens_list]
 #         labels = [self.tokenizer.decode(labels) for labels in labels_list]
@@ -69,12 +72,12 @@ class DataCollatorCustom:
 #             labels, return_tensors='pt', truncation=True,
 #             padding='max_length', max_length=self.max_length)
         
-#         encodings["input_ids"] = torch.tensor(tokens_list)
-#         encodings["attention_mask"] = torch.tensor(attention_masks_list)
+        encodings["input_ids"] = torch.tensor(tokens_list)
+        encodings["attention_mask"] = torch.tensor(attention_masks_list)
 
-        encodings = self.tokenizer(
-            tokens_list, return_tensors='pt', truncation=True,
-            padding='max_length', max_length=self.max_length)
+#         encodings = self.tokenizer(
+#             tokens_list, return_tensors='pt', truncation=True,
+#             padding='max_length', max_length=self.max_length)
         encodings["labels"] = torch.tensor(labels_list)
         
 #         print("encodings:\n", encodings)
@@ -87,9 +90,9 @@ class DataCollatorCustom:
 
         return encodings
 
-    def formatting(self, target_text):
-#         input_tokens = self.tokenizer(input_text)["input_ids"]
-#         target_tokens = self.tokenizer(target_text)["input_ids"]
+    def formatting(self, input_text, target_text):
+        input_tokens = self.tokenizer(input_text)["input_ids"]
+        target_tokens = self.tokenizer(target_text)["input_ids"]
 
 #         tokens = [self.tokenizer.bos_token_id] + input_tokens \
 #                 + [self.tokenizer.eos_token_id]
@@ -99,28 +102,28 @@ class DataCollatorCustom:
 #         labels = labels[:self.max_length]
 
 #         input_tokens = self.tokenizer.encode(input_text)
-        target_tokens = self.tokenizer.encode(target_text)
+#         target_tokens = self.tokenizer.encode(target_text)
 
-#         tokens = [self.tokenizer.bos_token_id] + input_tokens \
-#             + [self.tokenizer.sep_token_id] + target_tokens \
-#             + [self.tokenizer.eos_token_id]
-#         tokens = tokens + [self.tokenizer.pad_token_id] * (self.max_length - len(tokens))
-#         tokens = tokens[:self.max_length]
+        tokens = [self.tokenizer.bos_token_id] + input_tokens \
+            + [self.tokenizer.sep_token_id] + target_tokens \
+            + [self.tokenizer.eos_token_id]
+        tokens = tokens + [self.tokenizer.pad_token_id] * (self.max_length - len(tokens))
+        tokens = tokens[:self.max_length]
         
-#         attention_masks = [1] + [1] * len(input_tokens) + [1] \
-#                         + [1] * len(target_tokens) \
-#                         + [0] * (self.max_length - len(tokens))
-#         attention_masks = attention_masks[:self.max_length]
+        attention_masks = [1] + [1] * len(input_tokens) + [1] \
+                        + [1] * len(target_tokens)  + [1] \
+                        + [0] * self.max_length
+        attention_masks = attention_masks[:self.max_length]
 
-#         labels = [-100] * (len(input_tokens) + 2) \
-#             + target_tokens + [self.tokenizer.eos_token_id] \
-#             + [-100] * (self.max_length - len(tokens))
-#         labels = labels[:self.max_length]
-        
-#         return tokens, attention_masks, labels
-        
-        labels = target_tokens + [self.tokenizer.eos_token_id] \
-            + [-100] * (self.max_length - len(target_tokens))
+        labels = [-100] * (len(input_tokens) + 2) \
+            + target_tokens + [self.tokenizer.eos_token_id] \
+            + [-100] * self.max_length
         labels = labels[:self.max_length]
+        
+        return tokens, attention_masks, labels
+        
+#         labels = target_tokens + [self.tokenizer.eos_token_id] \
+#             + [-100] * (self.max_length - len(target_tokens))
+#         labels = labels[:self.max_length]
 
-        return labels
+#         return labels
